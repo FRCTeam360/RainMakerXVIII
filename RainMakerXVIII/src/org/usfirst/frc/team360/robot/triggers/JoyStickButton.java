976a25dfdd45a0f93a2b1b9d0b0da1f0efb78365
poshamazing <poshamazing@gmail.com>
2017-02-23 16:58:35 -0800

    a little more cleaning

--- RainMakerXVIII/src/org/usfirst/frc/team360/robot/triggers/JoyStickButton.java
+++ RainMakerXVIII/src/org/usfirst/frc/team360/robot/triggers/JoyStickButton.java
@@ -3,20 +3,16 @@ package org.usfirst.frc.team360.robot.triggers;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 
-/**
- *
- */
 public class JoyStickButton extends Trigger {
-    	private Joystick joy;
-    	double threshold;
-    	int axis;
-    	public void DoubleButton(Joystick joy, int axis, double threshold) {
-    		this.joy = joy;
-    		this.threshold = threshold;
-    		this.axis = axis;
-    	}	
-    
-        public boolean get() {
-            return Math.abs(joy.getRawAxis(axis)) > Math.abs(axis);
-        }
+    private Joystick joy;
+    double threshold;
+   	int axis;
+    public void DoubleButton(Joystick joy, int axis, double threshold) {
+    	this.joy = joy;
+    	this.threshold = threshold;
+    	this.axis = axis;
+    }	
+    public boolean get() {
+    	return Math.abs(joy.getRawAxis(axis)) > Math.abs(axis);
     }
+}
