1b40371e12f4b98b630e33965ebaa4d1fa00f684
poshamazing <poshamazing@gmail.com>
2017-03-23 18:05:31 -0700

    added alternative drive methods for eval

---
+++ RainMakerXVIII/src/org/usfirst/frc/team360/robot/commands/CheesyDrive.java
@@ -0,0 +1,95 @@
+package org.usfirst.frc.team360.robot.commands;
+
+import org.usfirst.frc.team360.robot.OI;
+import org.usfirst.frc.team360.robot.Robot;
+import org.usfirst.frc.team360.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * from 254
+ */
+public class CheesyDrive extends Command {
+
+    public CheesyDrive() {
+     	requires(Robot.drivetrain); 
+    }
+
+    protected void initialize() {
+    }
+    
+    protected void execute() {
+		cheesyDrive(OI.joyR.getRawAxis(1), OI.joyL.getRawAxis(0), OI.joyL.getRawButton(4));
+    }
+    double mQuickStopAccumulator;
+    public static final double kThrottleDeadband = 0.02;
+    private static final double kWheelDeadband = 0.02;
+    private static final double kTurnSensitivity = 1.0;
+    
+    public static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1);
+    }
+
+    public void cheesyDrive(double throttle, double wheel, boolean isQuickTurn) {
+
+        wheel = handleDeadband(wheel, kWheelDeadband);
+        throttle = handleDeadband(throttle, kThrottleDeadband);
+
+        double overPower;
+
+        double angularPower;
+
+        if (isQuickTurn) {
+            if (Math.abs(throttle) < 0.2) {
+                double alpha = 0.1;
+                mQuickStopAccumulator = (1 - alpha) * mQuickStopAccumulator + alpha * limit(wheel, 1.0) * 2;
+            }
+            overPower = 1.0;
+            angularPower = wheel;
+        } else {
+            overPower = 0.0;
+            angularPower = Math.abs(throttle) * wheel * kTurnSensitivity - mQuickStopAccumulator;
+            if (mQuickStopAccumulator > 1) {
+                mQuickStopAccumulator -= 1;
+            } else if (mQuickStopAccumulator < -1) {
+                mQuickStopAccumulator += 1;
+            } else {
+                mQuickStopAccumulator = 0.0;
+            }
+        }
+
+        double rightPwm = throttle - angularPower;
+        double leftPwm = throttle + angularPower;
+        if (leftPwm > 1.0) {
+            rightPwm -= overPower * (leftPwm - 1.0);
+            leftPwm = 1.0;
+        } else if (rightPwm > 1.0) {
+            leftPwm -= overPower * (rightPwm - 1.0);
+            rightPwm = 1.0;
+        } else if (leftPwm < -1.0) {
+            rightPwm += overPower * (-1.0 - leftPwm);
+            leftPwm = -1.0;
+        } else if (rightPwm < -1.0) {
+            leftPwm += overPower * (-1.0 - rightPwm);
+            rightPwm = -1.0;
+        }
+        Robot.drivetrain.driveR(rightPwm);
+        Robot.drivetrain.driveL(leftPwm);
+    }
+
+    public double handleDeadband(double val, double deadband) {
+        return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
+    }
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    	Robot.drivetrain.stopR();
+    	Robot.drivetrain.stopL();
+    }
+    
+    protected void interrupted() {
+    	end();
+    }
+}
